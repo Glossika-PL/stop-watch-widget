e19d3f164b04c4b2a89adf811d18403decf856dd
Robinson Lin <am810603@gmail.com>
2024-03-15 16:18:59 +0800

    feat(page): Create stop watch app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,47 @@
+'use client'
+
+import Button from '@/components/buttons/buttons'
+import LapsComponent from '@/components/laps/laps-component'
+import { ButtonStyle } from '@/enum/component'
+import useCreateTimer from '@/hooks/use-create-timer'
+import '@/styles/stop-watch-app.scss'
+import formatTime from '@/utils/format-time'
+
 export default function StopWatchApp() {
+  const [onStart, onStop, onReset, onLap, recordTime, time, start] =
+    useCreateTimer()
+
   return (
-    <main>
-      {/* Put your components here */}
-      <h2>00:00.00</h2>
-      <button>Lap</button>
-      <button>Start</button>
+    <main className="main">
+      <h2>{formatTime(time)}</h2>
+      <div className="buttonGroup">
+        {(start || time === 0) && (
+          <Button
+            handleClick={onLap}
+            disabled={time === 0}
+            buttonStyle={ButtonStyle.Lap}
+          >
+            Lap
+          </Button>
+        )}
+        {!start && time > 0 && (
+          <Button handleClick={onReset} buttonStyle={ButtonStyle.Reset}>
+            Reset
+          </Button>
+        )}
+        <Button
+          handleClick={start ? onStop : onStart}
+          buttonStyle={start ? ButtonStyle.Stop : ButtonStyle.Start}
+        >
+          {start ? 'Stop' : 'Start'}
+        </Button>
+      </div>
+      <div className="lapsGroup">
+        {time > 0 &&
+          recordTime.map(({ id, time }) => {
+            return <LapsComponent key={id} id={id} time={time} />
+          })}
+      </div>
     </main>
   )
 }
